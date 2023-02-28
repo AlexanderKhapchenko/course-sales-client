6fd742b8ed930cfecc07e7ad563b16ace10a99a4
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-02-28 22:42:20 +0200

    feat: added svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                override: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+        titleProp: true,
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
