414dacfa298dc253eb92510aa0a08434914a64f9
alexanderkhapchenko <alexanderfox1337@gmail.com>
2023-03-01 18:56:05 +0200

    fix: added className to paragraph

--- src/components/paragraph/paragraph.tsx
+++ src/components/paragraph/paragraph.tsx
@@ -2,10 +2,10 @@ import cn from 'classnames';
 import styles from './paragraph.module.css';
 import { ParagraphProps } from '@/components/paragraph/paragraph.props';
 
-export const Paragraph: React.FC<ParagraphProps> = ({ size = 'm', children, ...props }) => {
+export const Paragraph: React.FC<ParagraphProps> = ({ size = 'm', children, className, ...props }) => {
   return (
     <p
-      className={cn(styles.p, {
+      className={cn(styles.p, className, {
         [styles.s]: size === 's',
         [styles.m]: size === 'm',
         [styles.l]: size === 'l',
